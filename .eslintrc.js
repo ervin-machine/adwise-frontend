a0dbbbc1b7b2f8758bb89830ebbb0e61be0bc416
ervin-machine <ervinhodzic1234@gmail.com>
2025-05-31 14:29:11 +0200

    Update eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// .eslintrc.js
+module.exports = {
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-img-element': 'off',
+    }
+  }
+  
