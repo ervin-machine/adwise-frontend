1170bf97cf81962ce203c79ebe746d6284804759
ervin-machine <ervinhodzic1234@gmail.com>
2025-05-31 15:36:26 +0200

    Update api url

--- src/config/APIUrl.ts
+++ src/config/APIUrl.ts
@@ -1,4 +1,4 @@
 //const API_URL = import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5000/api"; // Fallback for development
-const API_URL = "http://localhost:5000/api"; // Fallback for development
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api"; // Fallback for development
 
 export default API_URL;
